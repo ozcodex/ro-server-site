7dba103e1c46dad4160bc0e480478361aa8365e5
Oscar Bernal <oscarbernal93@gmail.com>
2021-06-17 18:15:24 -0500

    Update views

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const PORT = 3001;
 
 let app = express();
 
+app.set('views','./views');
 app.set('view engine','pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',(req, res) => {
-  res.render('index',{
+  res.render('home.pug',{
     title: 'Ragnarok Server',
     message: 'Oz & Bernalophitecus Ragnarok Server'
+  })
+})
+
+app.get('/db',(req, res) => {
+  res.render('db.pug',{
+    title: 'Ragnarok Server | Database',
+    message: 'Coming soon...'
   })
 })
 
